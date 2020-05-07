302c93f52f2983acfaf7d82b936187ee676d20ea
Julien Béranger <julien@strat.cc>
2020-05-07 14:49:11 +0200

    add DAI

--- PortageSalarial.sol
+++ PortageSalarial.sol
@@ -1,61 +1,82 @@
 pragma solidity ^0.6.0;
 
-contract PortageSalarial {
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
+contract PortageSalarial {
+    
     address payable worker;
     address public customer;
     address public funder;
-
-    uint public value;
-    uint public amount;
-    uint public duration;
-    uint public start;
+    
     bool public active;
-    uint public holdUntil;
-    uint public paid;
-    uint public delay;
-    uint public lastPaid;
+    
+    ERC20 public DAI;
+    
+    uint256 public value;
+    uint256 public amount;
+    uint256 public duration;
+    uint256 public start;
+    uint256 public holdUntil;
+    uint256 public paid;
+    uint256 public delay;
+    uint256 public lastPaid;
+    uint256 public maxDelay;
 
     constructor (
-        address payable _worker, address _customer, uint _duration) public payable {
+        address payable _worker, address _customer, uint256 _value, uint256 _duration, uint256 _maxDelay) public payable {
         funder = msg.sender;
         worker = _worker;
         customer = _customer;
         duration = _duration;
         active = true;
         start = now;
-        value = msg.value;
-        amount = value/duration;
-        holdUntil = now;
+        value = _value * 10 ** 18 ;
+        amount = value / duration;
         lastPaid = now;
+        maxDelay = _maxDelay;
+        DAI = ERC20(0x7cc7E7D8547a813077CD97DC00848C81F4d8bD0e);
+        
     }
-
+    
     function hold() public {
         require (active == true);
         require (msg.sender == customer);
         require (now >= holdUntil);
-        holdUntil = now + 24 hours;
+        require (maxDelay < delay);
+        holdUntil = now + 1 minutes;
         delay += 1;
-        if (paid+delay >= duration) {
+        if (paid + delay >= duration) {
             active = false;
         }
     }
-
+    
     function pay() public {
         require (active == true);
         require (now >= holdUntil);
-        require (now >= lastPaid + 23 hours);
-        worker.transfer(amount);
+        require (now >= lastPaid + 1 minutes);
+        DAI.transfer(worker, amount);
         lastPaid = now;
         paid += 1;
-        if (paid+delay >= duration) {
+        if (paid + delay >= duration) {
             active = false;
         }
     }
-
+    
     function sweep() public {
-        require (active == false);
+        require (active == false); 
         require (msg.sender == funder);
-        msg.sender.transfer(address(this).balance);
+        DAI.transfer(funder, address(this).balance);
+    }
+    
+    function timeLeft() public view returns(uint256) {
+        return lastPaid + 1 minutes - now;
+    }
+    
+    function checkHold() public view returns(uint256) {
+        return holdUntil - now;
+    }
+    
+    function daiBalance() public view returns (uint256) {
+        return DAI.balanceOf(address(this));
     }
 }
